f9828f3190d4e37444b13ae525bd8928dc55ad72
Paulo Henrique M de Morais <111690624+MarshallLiu-Dev@users.noreply.github.com>
2023-07-26 13:45:42 -0300

    fixed bug server

--- src/Tests/home/Create.test.ts
+++ src/Tests/home/Create.test.ts
@@ -1,13 +1,13 @@
 import request from 'supertest';
-import { testServer } from '../jest.setup';
+import { server } from '../jest.setup';
 import { Response } from 'supertest';
 import { describe } from 'node:test';
 
 describe('Home - Create', () => {
     it('Deve retornar status 200 e uma mensagem de sucesso', async () => {
-        const response: Response = await request(testServer).get('/');
+        const response = await server.get('/');
         expect(response.status).toEqual(200);
-        expect(response.body.message).toEqual('Success'); // Replace 'message' with the actual key for the success message in the response body
+        expect(response.body.message).toEqual('Success');
     });
 });
 
