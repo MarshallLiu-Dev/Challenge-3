7c5e668e87dcfa314b2c84b0760e5f00f0d3c3ed
Thiago Vieira <60758912+ThiagoVieira585@users.noreply.github.com>
2023-07-27 00:18:31 -0300

    change route tutor_id for /tutor

--- src/Routes/router.ts
+++ src/Routes/router.ts
@@ -8,7 +8,7 @@ const router: Router = Router();
 
 router.get("/", homeController.home);
 router.post("/tutors", tutorController.createTutor.bind(tutorController));
-router.get("tutors_id", tutorController.getTutors.bind(tutorController));
+router.get("/tutors", tutorController.getTutors.bind(tutorController));
 router.get("/tutors/:id", tutorController.getTutorById.bind(tutorController));
 router.delete("/tutors/:id", tutorController.deleteTutors.bind(tutorController));
 router.put("/tutors/:id", tutorController.updateTutors.bind(tutorController));
