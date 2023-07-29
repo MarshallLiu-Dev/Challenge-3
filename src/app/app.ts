a9abd3a387e252a4f686a5666d4981aa167e7b46
Paulo Henrique M de Morais <111690624+MarshallLiu-Dev@users.noreply.github.com>
2023-07-29 16:38:52 -0300

    test run Createtutor Bug in test UpdateTutor GetTutor

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,28 +1,3 @@
-// import express from 'express';
-// import { router } from '../Routes/router';
-
-// export class App {
-//     static listen() {
-//         throw new Error("Method not implemented.");
-//     }
-//     public server: express.Application;
-
-//     constructor() {
-//         this.server = express();
-//         this.middleware();
-//         this.router();
-//     }
-
-//     private middleware() {
-//         this.server.use(express.json());
-//     }
-
-//     private router() {
-//         this.server.use(router);
-//     }
-// }
-
-
 import express from 'express';
 import { router } from '../Routes/router';
 
